93504ec6f40c0bc224b170851cf425e42ee0f9cc
Nikita <github.com@potapkin.com>
2024-08-20 12:12:32 +0300

    Изменена обработка нажатия кнопок. Проблема с закрытием клавиатуры осталась.

--- app/src/main/java/com/example/checkboxsnackbarpartone/MainActivity.kt
+++ app/src/main/java/com/example/checkboxsnackbarpartone/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var inputDataET: EditText
     private lateinit var saveDataBTN: Button
     private lateinit var savedDataTV: TextView
@@ -33,25 +33,37 @@ class MainActivity : AppCompatActivity() {
         saveDataBTN = findViewById(R.id.saveDataBTN)
         savedDataTV = findViewById(R.id.savedDataTV)
         clearDataBTN = findViewById(R.id.clearDataBTN)
-        saveDataBTN.setOnClickListener { view ->
-            val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
-            savedDataTV.text = inputDataET.text.toString()
-            inputDataET.text = null
-        }
-        clearDataBTN.setOnClickListener { view ->
-            val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
-            if (savedDataTV.text == null || savedDataTV.text == "") {
-                Snackbar.make(view, "Данные отсуствуют", Snackbar.LENGTH_SHORT).show()
-            }else{
-            val alert = Snackbar
-                .make(view, "Подтвердите удаление:", Snackbar.LENGTH_SHORT)
-                .setAction("Удалить") {
-                    savedDataTV.text = null
-                    Snackbar.make(view, "Данные удалены", Snackbar.LENGTH_SHORT).show()
+        saveDataBTN.setOnClickListener(this)
+        clearDataBTN.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.saveDataBTN -> {
+                val imm =
+                    p0.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(p0.windowToken, 0)
+                savedDataTV.text = inputDataET.text.toString()
+                inputDataET.text = null
+            }
+
+            R.id.clearDataBTN -> {
+                val imm =
+                    p0.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(p0.windowToken, 0)
+
+                if (savedDataTV.text == null || savedDataTV.text == "") {
+                    Snackbar.make(p0, "Данные отсуствуют", Snackbar.LENGTH_SHORT).show()
+                } else {
+                    val alert = Snackbar
+                        .make(p0, "Подтвердите удаление:", Snackbar.LENGTH_SHORT)
+                        .setAction("Удалить") {
+                            savedDataTV.text = null
+                            Snackbar.make(p0, "Данные удалены", Snackbar.LENGTH_SHORT).show()
+                        }
+                    alert.setActionTextColor(Color.RED).show()
                 }
-            alert.setActionTextColor(Color.RED).show()}
+            }
         }
     }
 }
